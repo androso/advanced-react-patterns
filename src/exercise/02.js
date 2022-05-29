d81d876d19cc0a72094a4e9f8049492a6dd3e534
Anibal <androso421@gmail.com>
2022-05-29 11:55:54 -0600

    finishing compound components

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,41 +4,34 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+const App = () => {
+  return (
+    <Toggle>
+      <ToggleOn>Toggle is On</ToggleOn>
+      <ToggleOff>Toggle is off</ToggleOff>
+      <ToggleButton />
+    </Toggle>
+  )
+}
+
+const Toggle = ({children}) => {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(children, child => {
+    return typeof child.type === 'string'
+      ? child
+      : React.cloneElement(child, {on, toggle})
+  })
 }
 
-// 🐨 Flesh out each of these components
+const ToggleOn = ({on, children}) => on ? children : null;
+const ToggleOff = ({on, children}) => on ? null : children;
 
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
-
-function App() {
-  return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <ToggleButton />
-      </Toggle>
-    </div>
-  )
+const ToggleButton = ({toggle, on}) => {
+  return <Switch onClick={toggle} on={on} /> 
 }
+
 
 export default App
 
